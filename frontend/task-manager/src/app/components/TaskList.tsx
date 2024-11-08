323de6bb539e28f1245b1cfe5748d9dc1bcb4af4
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-11-08 11:51:58 +0200

    Add TaskList component to display and manage a list of tasks

---
+++ frontend/task-manager/src/app/components/TaskList.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Task from "./Task";
+
+interface TaskListProps{
+  taskNames:string[];
+  selectedTaskIndex: number;
+  onTaskSelect: (index:number)=>void;
+  closeEditMode: ()=>void;
+}
+
+const TaskList = ({taskNames, selectedTaskIndex, onTaskSelect, closeEditMode}:TaskListProps) => {
+  return (
+    <div className="flex lg:flex-col w-full flex-row gap-[0.7rem] task-list lg:w-fit lg:h-[580px] overflow-auto">
+      {taskNames?.map((name, index) => {
+        return <Task key={index} selected={index===selectedTaskIndex} closeEditMode={closeEditMode} name={name} onClick={()=>onTaskSelect(index)} />;
+      })}
+    </div>
+  );
+};
+
+export default TaskList;
